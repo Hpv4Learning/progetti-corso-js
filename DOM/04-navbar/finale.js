da2cbc3ab586440dea57a9817b6b1d64a1392fb5
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2021-10-18 20:44:47 +0200

    added comments

--- DOM/04-navbar/finale.js
+++ DOM/04-navbar/finale.js
@@ -5,5 +5,13 @@ const navLinks = document.querySelector(".nav-links");
 
 //evento sul bottone navToggle che aggiunge/elimina la classe show-links
 navToggle.addEventListener("click", function () {
+  // console.log(navLinks.classList);
+  // console.log(navLinks.classList.contains("random"));
+  // console.log(navLinks.classList.contains("nav-links"));
+  // if (navLinks.classList.contains("show-links")) {
+  //   navLinks.classList.remove("show-links");
+  // } else {
+  //   navLinks.classList.add("show-links");
+  // }
   navLinks.classList.toggle("show-links");
 });
