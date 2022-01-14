5f070189e720ee488153733aa5596b219ffa633f
leonardodemarco <75417736+leonardodemarco@users.noreply.github.com>
2022-01-14 09:22:51 +0100

    fix: change api

--- AJAX/01-chuck-norris/finale.js
+++ AJAX/01-chuck-norris/finale.js
@@ -1,18 +1,18 @@
 const btn = document.querySelector(".button-xs");
 const text = document.querySelector(".text");
 const img = document.querySelector(".container img");
-const URL = "https://api.chucknorris.io/jokes/random";
+const URL = "https://catfact.ninja/fact";
 
 btn.addEventListener("click", async () => {
   try {
     const data = await fetch(URL);
     const response = await data.json();
-    displayData(response);
+    displayData(response.fact);
   } catch (error) {
     console.log(error);
   }
 });
 
-function displayData({ value: joke }) {
-  text.textContent = joke;
+function displayData(value) {
+  text.textContent = value;
 }
